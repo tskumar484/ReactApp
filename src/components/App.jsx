608d1395149b82225bd49d2f0720f846500891af
Tumala <ST029508@cerner.net>
2018-06-19 06:37:58 +0530

    Add: Lifecycle methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import './App.css';
 
 export default class App extends React.Component {
-  render() {
-    return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
-  }
+
+    constructor(props) {
+        console.log('I am in constructor!');
+        super(props);
+        this.state = {
+            data: 'Hi Santhosh!'
+        }
+    }
+
+    componentWillMount() {
+        console.log('I am in componentWillMount!');
+    }
+
+    getData() {
+        setTimeout(() => {
+            console.log('I am in getData method!');
+            this.setState({
+                data: 'Hello Susmitha'
+            })
+        }, 2000)
+    }
+
+    componentDidMount() {
+        console.log('I am in componentDidMount!');
+        this.getData();
+    }
+
+    render() {
+        console.log('I am in render!');
+        return (
+            <div className="container">
+                <h1>{this.props.name}:</h1>
+                <h2>{this.state.data}</h2>
+            </div>
+        )
+    }
 }
--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App name="Output"/>, document.getElementById('root'));
